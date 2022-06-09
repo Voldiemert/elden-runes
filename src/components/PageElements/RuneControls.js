f9158e7bfbdb382e97745b8cb99154f3d7468621
Voldiemert <ryan.volmert@gmail.com>
2022-06-08 22:03:18 -0500

    fix some max number errors; handle onBlur and some basic positioning of things

--- src/components/PageElements/RuneControls.js
+++ src/components/PageElements/RuneControls.js
@@ -1,15 +1,18 @@
 import {useState} from "react";
 import styles from './RuneControls.module.scss';
 
-const RuneControls = () => {
+const RuneControls = (props) => {
     let [runeCount, setRuneCount] = useState(0);
 
     const updateRuneCountHandler = (event) => {
-        if (event.target.value > 699) {
+        if (event.target.value > 99) {
             console.log('hold up slayer, you can\'t hold that many runes in-game');
         }
         else if (event.target.value < 0) {
-            console.log('bank account balances are invalid. Try 0-699');
+            console.log('bank account balances are invalid. Try 0-99');
+        }
+        else if (event.target.value === "") {
+            setRuneCount(0);
         }
         else {
             setRuneCount(event.target.value);
@@ -17,22 +20,30 @@ const RuneControls = () => {
     }
 
     const decrementRuneCountHandler = () => {
-        if (runeCount >= 1 && runeCount <= 699) {
+        if (runeCount >= 1 && runeCount <= 99) {
             setRuneCount(--runeCount);
         }
     }
 
     const incrementRuneCountHandler = () => {
-        if (runeCount >= 0 && runeCount < 699) {
+        if (runeCount >= 0 && runeCount < 99) {
             setRuneCount(++runeCount);
         }
     }
 
     return (
         <div>
-            <button onClick={decrementRuneCountHandler}>decrement</button>
-            <input className={styles.textCenter} type="number" inputMode="numeric" value={runeCount} min={0} max={999} onChange={updateRuneCountHandler}></input>
-            <button onClick={incrementRuneCountHandler}>increment</button>
+            <button onClick={decrementRuneCountHandler}>-</button>
+            <input className={styles.textCenter}
+               type="number"
+               inputMode="numeric"
+               value={runeCount}
+               min={0}
+               max={99}
+               onChange={updateRuneCountHandler}
+               onBlur={updateRuneCountHandler}>
+            </input>
+            <button onClick={incrementRuneCountHandler}>+</button>
         </div>
     )
 };
