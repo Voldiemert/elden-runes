c92dea05c0ae0cde35a1f8e262ec3145cd8c43be
Voldiemert <ryan.volmert@gmail.com>
2022-06-11 13:16:37 -0500

    create base rune summary component

---
+++ src/components/PageElements/RuneSummary.js
@@ -0,0 +1,11 @@
+import styles from './RuneSummary.module.scss';
+
+const RuneSummary = (props) => {
+    return (
+        <div>
+            <p>Rune summary</p>
+        </div>
+    );
+};
+
+export default RuneSummary;
