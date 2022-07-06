da4c3f1531150dc39389b8d20449dc1e4fdb961d
Voldiemert <ryan.volmert@gmail.com>
2022-07-05 23:37:29 -0500

    move runes needed calulation to StickyFooter; implement custom InputElement

--- src/components/UI/StickyFooter.js
+++ src/components/UI/StickyFooter.js
@@ -1,14 +1,30 @@
 import styles from './StickyFooter.module.scss';
+import {useState} from "react";
+import InputElement from "./InputElement";
 
 const StickyFooter = (props) => {
-    // x = ((Lvl+81)-92)*0.02
-    // x cannot be below 0, so change it to 0 if the formula resolves to below 0
-    // Rune Cost = ((x+0.1)*((Lvl+81)^2))+1
-    // The resulting number is always rounded down
+
+    const [currentLevel, setCurrentLevel] = useState(10);
+    // let currentLevel = 161;
+    const x = ((currentLevel+81)-92)*0.02;
+    // console.log(x);
+    const runeCost = Math.floor(((x+0.1)*((currentLevel+81)**2))+1);
+    // console.log(runeCost);
+
+    const update = (event) => {
+        // setCurrentLevel(currentLevel + 1);
+    }
 
     return (
         <footer className={styles.footer}>
-            {0}
+           <InputElement
+               type={"number"}
+               inputMode={"numeric"}
+               value={currentLevel}
+               min={10}
+               max={999}
+               onCustomElementEvent={update}
+           />
         </footer>
     );
 };
