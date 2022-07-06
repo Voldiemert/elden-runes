02fe58c37d5f1185937db742cd30e9a180ba31df
Voldiemert <ryan.volmert@gmail.com>
2022-07-05 23:43:01 -0500

    fix some bugggz related to custom InputElement

--- src/components/UI/StickyFooter.js
+++ src/components/UI/StickyFooter.js
@@ -11,20 +11,22 @@ const StickyFooter = (props) => {
     const runeCost = Math.floor(((x+0.1)*((currentLevel+81)**2))+1);
     // console.log(runeCost);
 
-    const update = (event) => {
+    const onCustomInputElementEvent = (event) => {
+        console.log('event');
         // setCurrentLevel(currentLevel + 1);
     }
 
     return (
         <footer className={styles.footer}>
-           <InputElement
-               type={"number"}
-               inputMode={"numeric"}
-               value={currentLevel}
-               min={10}
-               max={999}
-               onCustomElementEvent={update}
-           />
+            <InputElement
+                type={"number"}
+                inputMode={"numeric"}
+                value={currentLevel}
+                min={10}
+                max={999}
+                onCustomInputElementEvent={onCustomInputElementEvent}
+            />
+            {runeCost}
         </footer>
     );
 };
