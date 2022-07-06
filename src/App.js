ee25874cc665216364b24c74d1b7d54883ac8b33
Voldiemert <ryan.volmert@gmail.com>
2022-07-06 16:44:15 -0500

    move the total rune count to the app directory so it can be used in other components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.scss';
 import Container from "./components/UI/Container";
 import StickyFooter from "./components/UI/StickyFooter";
+import {useState} from "react";
 
 function App() {
+    const [totalRunes, setTotalRunes] = useState(0);
+
+    const updateTotalRunes = (total) => {
+        setTotalRunes(total);
+    };
+
     return (
         <div className="App">
             <header className="App-header">
                 <p>Elden Runes</p>
             </header>
-            <Container />
-            <StickyFooter />
+            <Container totalRunes={totalRunes} updateTotalRunes={updateTotalRunes} />
+            <StickyFooter totalRunes={totalRunes} />
         </div>
     );
 }
