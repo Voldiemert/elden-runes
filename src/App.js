e18b8d4b38b20466515ba5f25110b5968e7cd1f7
Voldiemert <ryan.volmert@gmail.com>
2022-08-29 23:58:48 -0500

    work on background image

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
             <header className="App-header">
                 <p>Elden Runes</p>
             </header>
+            <div className="backgroundImage"></div>
             <Container totalRunes={totalRunes} updateTotalRunes={updateTotalRunes} />
             <StickyFooter totalRunes={totalRunes} />
         </div>
